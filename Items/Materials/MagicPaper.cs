4dc002ad47cf57df6cdd52bd518c01466b974a8a
MestreSol <joaovitorferreira8000@gmail.com>
2021-04-13 21:53:01 -0300

    Adicionar arquivos de projeto.

---
+++ Items/Materials/MagicPaper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Localization;
+using static Terraria.ModLoader.ModContent;
+namespace UNJ.Items.Materials
+{
+    class MagicPaper : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Pergaminho magico");
+            Tooltip.SetDefault("Escritas antigas fazem com que este pergaminho emita uma força estranha.");
+
+            //Tradução
+            DisplayName.AddTranslation(GameCulture.English, "Magic papyrus");
+            Tooltip.AddTranslation(GameCulture.English, "Ancient writing make this papyrus send a strange force.");
+        }
+        public override void SetDefaults()
+        {
+            item.material = false;
+            item.width = 20;
+            item.height = 20;
+            item.maxStack = 999;
+            item.rare = 6;
+
+            item.value = Item.sellPrice(platinum: 0, gold: 0, silver: 0, copper: 50);
+            item.value = Item.buyPrice(platinum: 0, gold: 0, silver: 1, copper: 0);
+        }
+    }
+}
---
+++ Items/Pet/PaperAirplane.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Localization;
+using static Terraria.ModLoader.ModContent;
+using UNJ.Items.Materials;
+
+namespace UNJ.Items.Pet
+{
+    class PaperAirplane : ModItem
+    {
+		public override void SetStaticDefaults()
+		{
+			// DisplayName and Tooltip are automatically set from the .lang files, but below is how it is done normally.
+			DisplayName.SetDefault("Aviãozinho de papel");
+			Tooltip.SetDefault("Evoque um avião de papel magico que te seguirá para sempre");
+		}
+
+		public override void SetDefaults()
+		{
+			item.CloneDefaults(ItemID.ZephyrFish);
+			item.shoot = ProjectileType<Projectiles.Pets.PaperAirplane>();
+			item.buffType = BuffType<Buffs.PaperAirplane>();
+			
+			item.value = Item.sellPrice(platinum: 1);
+			item.value = Item.buyPrice(platinum: 100);
+
+		}
+
+		public override void AddRecipes()
+		{
+			
+			ModRecipe recipe = new ModRecipe(mod); // Gera uma receita para o item atual.
+			recipe.AddIngredient(ItemType<MagicPaper>(),25);
+			recipe.AddTile(36);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+
+		}
+
+		public override void UseStyle(Player player)
+		{
+			if (player.whoAmI == Main.myPlayer && player.itemTime == 0)
+			{
+				player.AddBuff(item.buffType, 3600, true);
+			}
+		}
+	}
+}
